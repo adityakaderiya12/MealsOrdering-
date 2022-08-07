e6452faf06d6ac72466e5b4690cba39d217a497f
adityakaderiya12 <adityakaderiya12@gmail.com>
2022-08-07 21:32:39 +0545

    Render CategoryGridTile with color on landup

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StyleSheet } from "react-native";
+import { StatusBar } from "react-native-web";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default  function App(){
   return(
+
+    <>
+    <StatusBar style= 'light'/>
+
     <CategoriesScreen/>
+    </>
   );
 }
 const styles =StyleSheet.create({
